bdbec3a2e98ae68027e05f2f010c3c15296494f6
Luis Lau <theauthorgm@gmail.com>
2018-09-03 19:52:33 -0500

    add mobile navbar functionality

---
+++ client/scripts/base.js
@@ -0,0 +1,24 @@
+
+// must commit index page today
+
+let mtNavHidden = false;
+function toggleMobileNav() {
+	const nav = $('nav.responsive-mobile');
+	if(mtNavHidden)
+		nav.slideDown(250, 'easeOutCubic').prop('hidden', false);
+	else
+		nav.slideUp(250, 'easeOutCubic').prop('hidden', true);
+	mtNavHidden = !mtNavHidden;
+}
+
+function handleControls() {
+	toggleMobileNav();
+	$('.nav-control').click(function(evt) {
+		evt.preventDefault();
+		toggleMobileNav();
+	});
+}
+
+$(function() {
+	handleControls();
+});
