8cde94141526da78c0130dedca087db30dc43d28
Luis Lau <theauthorgm@gmail.com>
2018-09-04 13:45:07 -0500

    add test for error page

--- test/test-client.js
+++ test/test-client.js
@@ -7,26 +7,35 @@ chai.use(require('chai-http'));
 const expect = chai.expect;
 
 describe('Meal Tracker Client Gateway', function() {
-    
-    beforeEach(() => startServer());
-    afterEach(() => stopServer());
+	
+	beforeEach(() => startServer());
+	afterEach(() => stopServer());
 
-    it('should serve the landing page on /', function() {
-        return chai.request(app)
-            .get('/')
-            .then(function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.html;
-            });
-    });
+	it('should serve the landing page on /', function() {
+		return chai.request(app)
+			.get('/')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
 
-    it('should serve the signup page on /signup', function() {
-        return chai.request(app)
-            .get('/signup')
-            .then(function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.html;
-            });
-    });
+	it('should serve the signup page on /signup', function() {
+		return chai.request(app)
+			.get('/signup')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+
+	it('should serve the error page on any other endpoint', function() {
+		return chai.request(app)
+			.get('/fake-endpoint')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
 
 });
