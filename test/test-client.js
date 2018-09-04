d153fffeb9d5c107ff055d55024bb3c0a963f039
Luis Lau <theauthorgm@gmail.com>
2018-09-04 13:27:11 -0500

    add test for signup page

--- test/test-client.js
+++ test/test-client.js
@@ -11,9 +11,18 @@ describe('Meal Tracker Client Gateway', function() {
     beforeEach(() => startServer());
     afterEach(() => stopServer());
 
-    it('should serve the landing page', function() {
+    it('should serve the landing page on /', function() {
         return chai.request(app)
             .get('/')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.html;
+            });
+    });
+
+    it('should serve the signup page on /signup', function() {
+        return chai.request(app)
+            .get('/signup')
             .then(function(res) {
                 expect(res).to.have.status(200);
                 expect(res).to.be.html;
