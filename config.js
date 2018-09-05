b102bf38fc872724063c5ed6858746086c79f2c0
Luis Lau <theauthorgm@gmail.com>
2018-09-05 15:18:12 -0500

    add jwt options to config file

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	PORT: process.env.PORT || 8080,
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/testdb',
-	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/testdb'
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/testdb',
+	JWT_SECRET: '123abc', // Replace this with a strong, gibberish string!
+	JWT_EXPIRY: '1d'
 };
