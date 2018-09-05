c78906b9e0368de9518c3959439a8d3dd545e35e
Luis Lau <theauthorgm@gmail.com>
2018-09-05 18:26:02 -0500

    update client router

--- client/router-client.js
+++ client/router-client.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {jwtAuth} = require('../auth/auth');
 const router = require('express').Router();
 
 function gotoPage(res, page) {
@@ -10,25 +11,16 @@ router.get('/', (req, res) => {
 	gotoPage(res, 'index');
 });
 
-router.get('/:action', (req, res) => {
-	switch(req.params.action) {
-		case 'signup':
-			gotoPage(res, 'signup');
-			break;
-		case 'signin':
-			break;
-		case 'dashboard':
-			gotoPage(res, 'dashboard');
-			break;
-		case 'addentry':
-			break;
-		case 'journal':
-			gotoPage(res, 'journal');
-			break;
-		default:
-			gotoPage(res, 'error');
-			break;
-	}
-})
+router.get('/signup', (req, res) => {
+	gotoPage(res, 'signup');
+});
+
+router.get('/signin', (req, res) => {
+	gotoPage(res, 'signin');
+});
+
+router.get('/dashboard', jwtAuth, (req, res) => {
+	gotoPage(res, 'dashboard');
+});
 
 module.exports = router;
