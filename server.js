308d98ca81a2cb62de05c7e9d49fd01e603800f7
Luis Lau <theauthorgm@gmail.com>
2018-09-05 13:53:19 -0500

    add cross origin middleware

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Cross Origin
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	next();
+});
+
 // Routers
 
 app.use('/styles', express.static('./client/styles/'));
