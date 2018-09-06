be405cb8490ea5ff811308f1d68f57f01b40ad19
Luis Lau <theauthorgm@gmail.com>
2018-09-06 10:01:36 -0500

    add signout gateway

--- client/router-client.js
+++ client/router-client.js
@@ -18,6 +18,10 @@ router.get('/signin', (req, res) => {
 	gotoPage(res, 'signin');
 });
 
+router.get('/signout', (req, res) => {
+	gotoPage(res, 'signout');
+});
+
 router.get('/dashboard', (req, res) => {
 	gotoPage(res, 'dashboard');
 });
