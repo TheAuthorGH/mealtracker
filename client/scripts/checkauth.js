2e568276a373d750020d0049f4d4c7b182115784
Luis Lau <theauthorgm@gmail.com>
2018-09-07 13:17:19 -0500

    checkout algirthm now uses refresh endpoint

--- client/scripts/checkauth.js
+++ client/scripts/checkauth.js
@@ -1,6 +1,25 @@
 function checkAuth() {
-	if(!Cookies.get('mt_jwt'))
-		window.location.href= '/signin'
+	const jwt = Cookies.get('mt_jwt');
+	if(!jwt) {
+		window.location.href= '/signin';
+		return;
+	}
+	$.ajax({
+		type: 'GET',
+		url: '/auth/refresh',
+		dataType: 'json',
+		contentType: 'application/json',
+		beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + jwt) }
+	})
+	.done((res) => {
+		Cookies.set('mt_jwt', res);
+	})
+	.fail((res) => {
+		if(res.status === 401)
+			window.location.href = '/signin';
+		else
+			window.location.href= '/error';
+	});
 }
 
 checkAuth();
