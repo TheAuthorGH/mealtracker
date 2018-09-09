7385eb2f2c2228834d09e0dab82b2de4f2462e4d
Luis Lau <theauthorgm@gmail.com>
2018-09-08 19:16:59 -0500

    add setting to disable auth

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/testdb',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/testdb',
 	JWT_SECRET: '123abc', // Replace this with a strong, gibberish string!
-	JWT_EXPIRY: '1d'
+	JWT_EXPIRY: '1d',
+	ENABLE_AUTH: true
 };
