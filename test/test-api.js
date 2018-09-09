d6de877e1103baace5ecc19ccc8ce20f5f821ef8
Luis Lau <theauthorgm@gmail.com>
2018-09-08 19:19:25 -0500

    add journals api tests

--- test/test-api.js
+++ test/test-api.js
@@ -6,11 +6,16 @@ const mongoose = require('mongoose');
 const faker = require('faker');
 const expect = chai.expect;
 
+const config = require('../config');
+config.ENABLE_AUTH = false;
+
 const Users = require('../persistence/model-users');
+const Journals = require('../persistence/model-journals');
 const {TEST_DATABASE_URL} = require('../config');
-const { app, startServer, stopServer} = require('../server');
+const { app, startServer, stopServer } = require('../server');
 
 const modelUser = fakeUser();
+const modelJournal = fakeJournal(modelUser);
 
 function fakeUser() {
 	return {
@@ -19,12 +24,19 @@ function fakeUser() {
 	};
 }
 
+function fakeJournal(userid) {
+	return {
+		user: userid,
+		title: faker.lorem.words()
+	};
+}
+
 function populateDb() {
-	return new Promise((resolve, reject) => {
-		for(let i = 0; i < 5; i++)
-			Users.create(fakeUser());
-		resolve();
-	});
+	const users = [];
+	for(let i = 0; i < 10; i++)
+		users.push(fakeUser());
+	return Users.insertMany(users)
+		.then(users => Journals.insertMany(users.map(u => fakeJournal(u._id))));
 }
 
 function dropDb() {
@@ -32,9 +44,9 @@ function dropDb() {
 }
 
 describe('Meal Tracker API', function() {
-	
+
 	before(() => startServer(TEST_DATABASE_URL));
-	beforeEach(populateDb);
+	beforeEach(() => populateDb());
 	afterEach(dropDb);
 	after(stopServer);
 
@@ -51,7 +63,7 @@ describe('Meal Tracker API', function() {
 				.then(function(res) {
 					expect(res).to.have.status(200);
 					expect(res).to.be.json;
-					expect(res.body.user).to.include.keys('id', 'email', 'journals');
+					expect(res.body.user).to.include.keys('id', 'email', 'verified');
 					expect(res.body.user.id).to.equal(user._id.toString());
 					expect(res.body.user.email).to.equal(user.email);
 				});
@@ -59,22 +71,39 @@ describe('Meal Tracker API', function() {
 
 		it('should create a new user on POST /users', function() {
 			return chai.request(app)
-				.post('/users/')
+				.post('/users')
 				.send(modelUser)
-				.then(res => {
+				.then(function(res) {
 					expect(res).to.have.status(201)
 					expect(res).to.be.json;
-					expect(res.body.user).to.include.keys('id', 'email', 'journals', 'verified');
+					expect(res.body.user).to.include.keys('id', 'email', 'verified');
 					expect(res.body.user.email).to.equal(modelUser.email);
-					expect(res.body.user.journals).to.be.an('array');
 					return Users.findById(res.body.user.id);
 				})
-				.then(user => {
+				.then(function(user) {
 					expect(user).to.not.be.null;
 					user = user.toObject();
-					expect(user).to.include.keys('_id', 'email', 'verified', 'password', 'journals');
+					expect(user).to.include.keys('_id', 'email', 'verified', 'password');
 					expect(user.email).to.equal(modelUser.email);
-					expect(user.journals).to.be.an('array');
+				});
+		});
+
+	});
+
+	describe('Journals API', function() {
+
+		it('should return a single journal on GET /journals?id=...', function() {
+			let journal;
+			return Journals.findOne()
+				.then(function(_journal) {
+					journal = _journal;
+					return chai.request(app)
+						.get('/journals?id=' + journal._id);
+				})
+				.then(function(res) {
+					expect(res).to.have.status(200);
+					expect(res).to.be.json;
+					expect(res.body.journal).to.include.keys('id', 'user', 'title', 'entryAmount');
 				});
 		});
 
