f6fe26d95ebfe8a975b0bb8e7bfd921fd7e167c8
Luis Lau <theauthorgm@gmail.com>
2018-09-09 20:07:42 -0500

    update config file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	PORT: process.env.PORT || 8080,
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/testdb',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/testdb',
-	JWT_SECRET: '123abc', // Replace this with a strong, gibberish string!
-	JWT_EXPIRY: '1d',
-	ENABLE_AUTH: true
+	JWT_SECRET: process.env.JWT_SECRET || '123abc',
+	JWT_EXPIRY: process.env.JWT_EXPIRY || '1d',
+	ENABLE_AUTH: process.env.ENABLE_AUTH || false
 };
