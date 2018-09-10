ddfb89a99fb6a2639add2f7c2f4991952645ec2c
Luis Lau <theauthorgm@gmail.com>
2018-09-09 20:20:07 -0500

    improve default config values

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	PORT: process.env.PORT || 8080,
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/testdb',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/testdb',
-	JWT_SECRET: process.env.JWT_SECRET || '123abc',
+	JWT_SECRET: process.env.JWT_SECRET || '123abc!@#',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '1d',
-	ENABLE_AUTH: process.env.ENABLE_AUTH || false
+	ENABLE_AUTH: process.env.ENABLE_AUTH || true
 };
