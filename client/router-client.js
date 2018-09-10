a43c0f9c4d20a234daf6576a6af13af9b930cd72
Luis Lau <theauthorgm@gmail.com>
2018-09-10 16:32:31 -0500

    improve client router

--- client/router-client.js
+++ client/router-client.js
@@ -1,33 +1,21 @@
-'use strict';
-
 const router = require('express').Router();
 
-function gotoPage(res, page) {
-	res.sendFile(`${__dirname}/${page}.html`);
-}
-
 router.get('/', (req, res) => {
-	gotoPage(res, 'index');
-});
-
-router.get('/signup', (req, res) => {
-	gotoPage(res, 'signup');
-});
-
-router.get('/signin', (req, res) => {
-	gotoPage(res, 'signin');
-});
-
-router.get('/signout', (req, res) => {
-	gotoPage(res, 'signout');
-});
-
-router.get('/dashboard', (req, res) => {
-	gotoPage(res, 'dashboard');
-});
-
-router.get('/error', (req, res) => {
-	gotoPage(res, 'error');
+	res.sendFile(`${__dirname}/index.html`);
 })
+
+router.get('/:action', (req, res, next) => {
+	switch(req.params.action) {
+		case 'dashboard':
+		case 'signin':
+		case 'signout':
+		case 'signup':
+		case 'error':
+			res.sendFile(`${__dirname}/${req.params.action}.html`);
+			break;
+		default:
+			next();
+	}
+});
 
 module.exports = router;
