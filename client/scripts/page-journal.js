a21b2929c591259b4b3b39a8cbd0ddf4996eb738
Luis Lau <theauthorgm@gmail.com>
2018-09-23 17:02:27 -0500

    add journal pagination controls

--- client/scripts/page-journal.js
+++ client/scripts/page-journal.js
@@ -5,6 +5,8 @@ const MT_JOURNAL = new URL(window.location).searchParams.get('id');
 if(MT_JOURNAL === null)
 	window.location.href = '/dashboard';
 
+let currentPage = 0;
+
 function clearEntryForm() {
 	$('.mt-journal-addentry').find('input, textarea').val('');
 }
@@ -36,11 +38,18 @@ function handleJournalControls() {
 		})
 		.fail(() => window.location.href = '/error');
 	});
+
+	$('.mt-journal-entries-pagination-prev').click(function() {
+		updateEntries(currentPage - 1);
+	});
+	$('.mt-journal-entries-pagination-next').click(function() {
+		updateEntries(currentPage + 1);
+	});
 }
 
-function updateEntries() {
-	$('.mt-journal-noentries, .mt-journal-entries, .mt-journal-addentry').hide().prop('hidden', true);
-	$('.mt-journal-entries > ul').empty();
+function updateEntries(page = currentPage) {
+	if(page < 0)
+		return;
 	$.ajax({
 		type: 'GET',
 		url: `/journals?id=${MT_JOURNAL}`,
@@ -53,11 +62,17 @@ function updateEntries() {
 		$('h2').text('Journal - ' + journal.title);
 		$.ajax({
 			type: 'GET',
-			url: `/journals/entries?id=${MT_JOURNAL}&perpage=10&page=0`,
+			url: `/journals/entries?id=${MT_JOURNAL}&perpage=10&page=${page}`,
 			contentType: 'application/json',
 			beforeSend: MT_AUTH_BEFORESEND
 		})
 		.done(res => {
+			if(page >= res.pages)
+				return;
+			$('.mt-journal-noentries, .mt-journal-entries, .mt-journal-addentry').hide().prop('hidden', true);
+			$('.mt-journal-entries > ul').empty();
+			currentPage = page;
+			$('.mt-journal-entries-pagination-counter').text(`${currentPage + 1}/${res.pages}`);
 			const entries = res.entries;
 			if(entries.length === 0) {
 				$('.mt-journal-noentries').show();
