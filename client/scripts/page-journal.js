dc626751744b5fd03fa1324165d261883c246acf
Luis Lau <theauthorgm@gmail.com>
2018-09-25 11:09:45 -0500

    revamp journal pagination

--- client/scripts/page-journal.js
+++ client/scripts/page-journal.js
@@ -11,6 +11,24 @@ function clearEntryForm() {
 	$('.mt-journal-addentry').find('input, textarea').val('');
 }
 
+function uploadJournalEntry(title, desc) {
+	$.ajax({
+		type: 'POST',
+		url: `/journals/entries?id=${MT_JOURNAL}`,
+		dataType: 'json',
+		contentType: 'application/json',
+		beforeSend: MT_AUTH_BEFORESEND,
+		data: JSON.stringify({
+			title: title.trim(),
+			description: desc
+		})
+	})
+	.done(() => {
+		console.log('Uploaded');
+	})
+	.fail(() => window.location.href = '/error');
+}
+
 function handleJournalControls() {
 	$('.mt-journal-addentry').hide().prop('hidden', true);
 	$('.mt-journal-addentry-open').click(function() {
@@ -39,12 +57,10 @@ function handleJournalControls() {
 		.fail(() => window.location.href = '/error');
 	});
 
-	$('.mt-journal-entries-pagination-prev').click(function() {
-		updateEntries(currentPage - 1);
+	$('.mt-journal-entries-pagination').on('click', '.mt-journal-entries-pagination-page', function() {
+		updateEntries(Number($(this).attr('mt-journal-page')));
 	});
-	$('.mt-journal-entries-pagination-next').click(function() {
-		updateEntries(currentPage + 1);
-	});
+
 }
 
 function updateEntries(page = currentPage) {
@@ -59,7 +75,7 @@ function updateEntries(page = currentPage) {
 	.done(res => {
 		const journal = res.journal;
 		$('title').text('MealTracker - ' + journal.title);
-		$('h2').text('Journal - ' + journal.title);
+		$('h2').first().text('Journal - ' + journal.title);
 		$.ajax({
 			type: 'GET',
 			url: `/journals/entries?id=${MT_JOURNAL}&perpage=10&page=${page}`,
@@ -69,15 +85,25 @@ function updateEntries(page = currentPage) {
 		.done(res => {
 			if(page >= res.pages)
 				return;
-			$('.mt-journal-noentries, .mt-journal-entries, .mt-journal-addentry').hide().prop('hidden', true);
-			$('.mt-journal-entries > ul').empty();
 			currentPage = page;
-			$('.mt-journal-entries-pagination-counter').text(`${currentPage + 1}/${res.pages}`);
+
+			$('.mt-journal-noentries, .mt-journal-entries, .mt-journal-addentry').hide().prop('hidden', true);
+			$('.mt-journal-entries > ul, .mt-journal-entries-pagination').empty();
+
+			for(let c = -2; c < 3; c++) {
+				const p = page + c;
+				let button;
+				if(p < 0 || p >= res.pages)
+					button = '<button class="mt-empty"></button>';
+				else
+					button = `<button class="mt-journal-entries-pagination-page${p == page ? ' mt-selected' : ''}" mt-journal-page="${p}">${p + 1}</button>`;
+				$('.mt-journal-entries-pagination').append(button);
+			}
+
 			const entries = res.entries;
 			if(entries.length === 0) {
 				$('.mt-journal-noentries').show();
 			} else {
-				entries.sort((e, f) => new Date(f.date) - new Date(e.date));
 				for(let e of entries)
 					$('.mt-journal-entries > ul').append(`<li mt-journal-entry-id="${e.id}"><span>${e.title}</span><time>${formatDate(new Date(e.date))}</time></li>`);
 				$('.mt-journal-entries').show();
