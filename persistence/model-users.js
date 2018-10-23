70f6b3651e52c03a96dd5728b66d993c80e2ce10
Luis Lau <theauthorgm@gmail.com>
2018-10-23 10:20:37 -0500

    prettier

--- persistence/model-users.js
+++ persistence/model-users.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    verified: {type: Boolean, required: true, default: false},
-    password: {type: String, required: true}
+	email: {type: String, required: true, unique: true},
+	verified: {type: Boolean, required: true, default: false},
+	password: {type: String, required: true}
 });
 
 userSchema.methods.serialize = function() {
-    return {
-        id: this._id,
-        email: this.email,
-        verified: this.verified
-    };
+	return {
+		id: this._id,
+		email: this.email,
+		verified: this.verified
+	};
 };
 
 const Users = mongoose.model('User', userSchema);
