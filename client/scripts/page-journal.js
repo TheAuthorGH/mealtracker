fd34ecec642d2c62be7cdfbabf9fc9dd1f90c08a
Luis Lau <theauthorgm@gmail.com>
2018-10-25 21:50:54 -0500

    implement entry updating

--- client/scripts/page-journal.js
+++ client/scripts/page-journal.js
@@ -34,9 +34,13 @@ function handleJournalControls() {
 	$('.mt-journal-addentry').submit(function(evt) {
 		evt.preventDefault();
 
+		const title = $('#mt-journal-addentry-title').val().trim();
+		const positive = $('#mt-journal-addentry-positive').is(':checked');
 		let description = $('#mt-journal-addentry-description').val().trim();
 		if(!description)
 			description = null;
+		else
+			description = description.trim();
 
 		$.ajax({
 			type: 'POST',
@@ -45,9 +49,9 @@ function handleJournalControls() {
 			contentType: 'application/json',
 			beforeSend: MT_AUTH_BEFORESEND,
 			data: JSON.stringify({
-				title: $('#mt-journal-addentry-title').val().trim(),
+				title: title,
 				description: description,
-				positive: $('#mt-journal-addentry-positive').is(':checked')
+				positive: positive
 			})
 		})
 		.done(() => {
@@ -72,8 +76,11 @@ function handleJournalControls() {
 
 	// Entry Controls
 
-	$('.mt-journal-entries > ul').on('click', '.mt-journal-entry-remove', function() {
-		const entryId = $(this).closest('li').attr('mt-journal-entry-id');
+	$('.mt-journal-entries').on('click', '.mt-journal-entry-expand', function() {
+		$(this).closest('.mt-journal-entries li').find('div:not(:first-child)').toggle();
+	});
+	$('.mt-journal-entries').on('click', '.mt-journal-entry-remove', function() {
+		const entryId = $(this).closest('.mt-journal-entries li').attr('mt-journal-entry-id');
 		$.ajax({
 			type: 'DELETE',
 			url: `/journals/entries?journalid=${MT_JOURNAL}&userid=${Cookies.get('mt_user')}&entryid=${entryId}`,
@@ -84,8 +91,42 @@ function handleJournalControls() {
 			updateInsights();
 		});
 	});
-	$('.mt-journal-entries > ul').on('click', '.mt-journal-entry-expand', function() {
-		$(this).closest('.mt-journal-entries > ul > li').find('div:not(:first-child)').toggle();
+	$('.mt-journal-entries').on('click', '.mt-journal-entry-edit', function() {
+		const li = $(this).closest('.mt-journal-entries li');
+		li.find('div.mt-journal-entry-details').hide().prop('hidden', true);
+		li.find('form.mt-journal-editentry').show().prop('hidden', false);
+	});
+
+	$('.mt-journal-entries').on('submit', '.mt-journal-editentry', function(evt) {
+		evt.preventDefault();
+
+		const entryId = $(this).closest('.mt-journal-entries li').attr('mt-journal-entry-id');
+
+		const title = $(`#mt-journal-editentry-title-${entryId}`).val().trim();
+		const positive = $(`#mt-journal-editentry-positive-${entryId}`).is(':checked');
+		let description = $(`#mt-journal-editentry-description-${entryId}`).val();
+		if(!description)
+			description = null;
+		else
+			description = description.trim();
+		
+		$.ajax({
+			type: 'PUT',
+			url: `/journals/entries?journalid=${MT_JOURNAL}&userid=${Cookies.get('mt_user')}&entryid=${entryId}`,
+			dataType: 'json',
+			contentType: 'application/json',
+			beforeSend: MT_AUTH_BEFORESEND,
+			data: JSON.stringify({
+				id: entryId,
+				title: title,
+				description: description.trim(),
+				positive: positive
+			})
+		})
+		.done(() => {
+			updateEntries();
+		})
+		.fail(() => window.location.href = '/error');
 	});
 }
 
@@ -137,11 +178,21 @@ function updateEntries(page = currentPage) {
 								<button class="mt-journal-entry-expand mt-button-square"><span class="fas fa-fw fa-eye"></span></button>
 							</div>
 							<div>
+
+								<form class="mt-journal-editentry" style="display: none;" hidden>
+									<label for="mt-journal-editentry-title-${e.id}"><span>Title </span><input type="text" id="mt-journal-editentry-title-${e.id}" maxlenght="20" value="${e.title}" required/></label>
+									<label for="mt-journal-editentry-description-${e.id}">Description</label>
+									<textarea id="mt-journal-editentry-description-${e.id}" maxlength="300">${e.description}</textarea>
+									<label for="mt-journal-editentry-positive-${e.id}"><input type="checkbox" id="mt-journal-editentry-positive-${e.id}"${e.positive ? " checked" : ""}/><span> Mark this entry as healthy</span></label>
+									<button type="submit">Update</button>
+								</form>
+
 								<div class="mt-journal-entry-details">
 									<time><span class="far fa-clock fa-fw"></span> ${formatDate(new Date(e.date))}</time>
 									<p><span class="fas fa-info-circle fa-fw"></span> ${e.description ? '<em>&#8220' + e.description + '&#8221</em>' : 'No description.'}</p>
 									${e.positive ? '<p class="mt-journal-entry-details-positive"><span class="fas fa-check fa-fw"></span> Marked as healthy</p>' : '<p class="mt-journal-entry-details-negative"><span class="fas fa-times fa-fw"></span> Not marked as healthy</p>'}
 								</div>
+
 								<div class="mt-journal-entry-controls">
 									<button class="mt-journal-entry-edit mt-button-square"><span class="fas fa-fw fa-pencil-alt"></span></button>
 									<button class="mt-journal-entry-remove mt-button-square"><span class="fas fa-fw fa-times"></span></button>
